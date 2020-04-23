a74c990bc5db63d5f0e3e8f488c0b7439e95e458
nicobuffa <nicobuffa94@gmail.com>
2020-04-23 12:07:29 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,14 @@
+let express = require('express')
+let http = require('http')
+let app = express()
+
+app.get('/categories', (req, res) => {
+  debugger;
+  let parser = require('simple-excel-to-json');
+  let doc = parser.parseXls2Json('data/Categorias.xlsx', { isNested: true });
+  res.status(200).send(doc)
+})
+
+http.createServer(app).listen(8001, () => {
+  console.log('Server started at http://localhost:8001');
+});
