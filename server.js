08eda17627f7cb38e29b07b98adce866a8b3f5b1
nicobuffa <nicobuffa94@gmail.com>
2020-04-24 00:10:52 -0300

    Access

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 let express = require('express')
 let http = require('http')
 let app = express()
+// Get the adodb module
+const ADODB = require('node-adodb');
+const connection = ADODB.open('Provider=Microsoft.Jet.OLEDB.4.0;Data Source=data/Fanaticos1.mdb;');
 
-app.get('/categories', (req, res) => {
-  debugger;
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
+
+app.get('/catalog/categories/access', (req, res) => {
+
+  connection
+    .query('SELECT * FROM Categorias')
+    .then(data => {
+      console.log(JSON.stringify(data, null, 2));
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+res.status(200).send("ok")
+
+})
+
+app.get('/catalog/categories', (req, res) => {
   let parser = require('simple-excel-to-json');
-  let doc = parser.parseXls2Json('data/Categorias.xlsx', { isNested: true });
+  let doc = parser.parseXls2Json('data/Categorias.xlsx', { isNested: true })[0];
   res.status(200).send(doc)
 })
 
